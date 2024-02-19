f5ca7c227f8e5df1b6392add56f891a4e2658457
Mubarak Abdullahi <120288041+4mubarak@users.noreply.github.com>
2024-02-19 11:31:38 +0100

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/wellpaper-image.jpg"
+            src="./assets/t-shirt-wallpaper.png"
             alt="Card"
             height={500}
           />
